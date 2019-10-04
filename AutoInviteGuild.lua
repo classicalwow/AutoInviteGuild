d7be3794d21e0bd8b8226729431920e50870f33c
Typescript <poolk008@qq.com>
2019-10-04 14:25:27 +0800

    update

--- AutoInviteGuild.lua
+++ AutoInviteGuild.lua
@@ -3,6 +3,7 @@ local rotenable = false
 local annoenable = false
 local ticktime=60;
 local runtick=GetTime()
+local count = 0
 AutoInviteGroup = addonTable
 
 function print(msg) DEFAULT_CHAT_FRAME:AddMessage(msg) end
@@ -17,10 +18,11 @@ function timerproc()
 		if (runtick <= GetTime()) then
 			runtick = GetTime() + ticktime
 			local ids = {}
+			count = count+1
 			for key,value in ipairs({"大脚世界频道","世界频道","寻求组队","综合","交易","公会招募","世界防务"}) do
 				id = GetChannelName(value)
 				table.insert(ids,id)
-				SendChatMessage("《Hello World》公会诚招各路IT人士，打造一个和谐，友爱，文明的公会，将于10月下旬打造一个团结高效的纯DKP团队，活动时间每周六下午1-5点，欢迎IT人士及其朋友的加盟,扰屏见谅", "CHANNEL", nil, id)
+				SendChatMessage("{三角}{三角}《Hello World》公会诚招各路IT精英，打造一个和谐，友爱，文明的公会，将于10月下旬打造一个团结高效的纯DKP团队，活动时间每周六下午1-5点，扰屏见谅{三角}{三角}"..count, "CHANNEL", nil, id)
 			end
 			print("已在"..table.concat(ids,",").."频道中喊话");
 			
--- test.lua
+++ test.lua
@@ -1,4 +1,7 @@
 local a = {}
+local b = 0
 table.insert(a, 11)
 table.insert(a, "bar")
+b = b+1
+print('1'..b)
 print(table.concat(a,","))
